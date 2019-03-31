b03b3471b19d025462a243e0de19d6d00ea1cb8b
ajbrusic <ajbrusic@buffalo.edu>
2019-03-30 20:36:41 -0400

    updated player

--- MMOScala/src/MMO/Player/player.scala
+++ MMOScala/src/MMO/Player/player.scala
@@ -1,6 +1,13 @@
 package MMO.Player
 
-class player {
+import MMO.SateOfPlayer.state
+
+class player(var x: Int, var y: Int, val id: String){
+  var speed: Int = 200
+  val width: Int = 50
+  val height: Int = 50
+
+
   var LPressed = false
   var Rpressed = false
   var UpPressed = false
